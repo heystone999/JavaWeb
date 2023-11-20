f39891dd855ec27b5235897dd8fe2031c301a0e0
Jie Zhao <j79zhao@uwaterloo.ca>
2023-11-20 13:03:51 -0500

    Interceptor fot login

--- stone_web_project/tlias-web-management/src/main/java/com/stone/interceptor/LoginCheckInterceptor.java
+++ stone_web_project/tlias-web-management/src/main/java/com/stone/interceptor/LoginCheckInterceptor.java
@@ -1,38 +1,33 @@
-package com.stone.filter;
+package com.stone.interceptor;
 
 import com.alibaba.fastjson.JSONObject;
 import com.stone.pojo.Result;
 import com.stone.utils.JwtUtils;
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
-
+@Component
 @Slf4j
-//@WebFilter(urlPatterns = "/*")
-public class LoginCheckFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) servletRequest;
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override //目标资源方法运行前运行，true放行，false不放行
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
 //        获取请求url
-        String url = req.getRequestURL().toString();
+        String url = request.getRequestURL().toString();
         log.info("请求的url: {}", url);
 
 //        判断请求URL是否包含token？包含 登录 放行
         if (url.contains("login")) {
             log.info("登录操作 放行");
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
+            return true;
         }
 //        获取请求头中的token
-        String jwt = req.getHeader("token");
+        String jwt = request.getHeader("token");
 
 //        判断token是否存在 error
         if (!StringUtils.hasLength(jwt)) {
@@ -40,8 +35,8 @@ public class LoginCheckFilter implements Filter {
             Result error = Result.error("NOT_LOGIN");
             // 手动转换obj->json  用阿里fastJSON
             String notLogin = JSONObject.toJSONString(error);
-            resp.getWriter().write(notLogin);
-            return;
+            response.getWriter().write(notLogin);
+            return false;
         }
 
 //        解析token error
@@ -53,12 +48,21 @@ public class LoginCheckFilter implements Filter {
             Result error = Result.error("NOT_LOGIN");
             // 手动转换obj->json  用阿里fastJSON
             String notLogin = JSONObject.toJSONString(error);
-            resp.getWriter().write(notLogin);
-            return;
+            response.getWriter().write(notLogin);
+            return false;
         }
 //        放行
         log.info("token合法 放行");
-        filterChain.doFilter(servletRequest, servletResponse);
+        return true;
+    }
 
+    @Override ////目标资源方法运行后运行
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle ...");
+    }
+
+    @Override //视图渲染完毕后运行，最后运行
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion ...");
     }
 }
